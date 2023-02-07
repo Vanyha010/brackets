739f2a123b36a87fcc8b8c5eb27d2881f8ee8eeb
KotyaLapkin <kotyalapkin@yahoo.com>
2023-02-07 10:35:48 +0300

    feat: make the function that check if the brackets opened and closed correctly

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let objPairs = {};
+  let openBrackets = [];
+  for (let item of bracketsConfig) {
+    objPairs[item[0]] = item[1];
+    openBrackets.push(item[0]);
+  }
+
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    if (objPairs[stack[stack.length - 1]] == str[i]) {
+      stack.pop()
+    } else if (openBrackets.includes(str[i])) {
+      stack.push(str[i])
+    } else {
+      return false
+    }
+  }
+  return (stack.length == 0)
 }
